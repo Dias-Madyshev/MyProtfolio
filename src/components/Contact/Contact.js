71ee3c91437b188244725138c9c205b03235aa6e
Dias-Madyshev <madyshev_d@mail.ru>
2025-02-22 17:13:44 +0500

    Добавил изменения в проект

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -41,11 +41,14 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/send-message", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://backend-portfolio-lxpp.onrender.com/send-message",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json();
 
